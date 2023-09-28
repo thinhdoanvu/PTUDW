3cfb018c591894b921c9052240d1f3124ab36f42
thinhdoanvu <thinhdv@ntu.edu.vn>
2023-09-28 19:52:10 +0700

    Bai2_EntityFramework_63CNTT4N2

--- 63CNTT4N2/63CNTT4N2/Controllers/SiteController.cs
+++ 63CNTT4N2/63CNTT4N2/Controllers/SiteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyClass.Model;
 
 namespace _63CNTT4N2.Controllers
 {
@@ -11,6 +12,7 @@ namespace _63CNTT4N2.Controllers
         // GET: Site
         public ActionResult Index()
         {
+            
             return View();
         }
     }
