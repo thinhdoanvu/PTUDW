2949da317f58ef4f1f4fee9b015e9f8b7a6590a7
thinhdoanvu <thinhdv@ntu.edu.vn>
2023-10-04 00:30:24 +0700

    Index_Categories

--- 63CNTT4N1/Areas/Admin/Controllers/CategoriesController.cs
+++ 63CNTT4N1/Areas/Admin/Controllers/CategoriesController.cs
@@ -13,13 +13,14 @@ namespace _63CNTT4N1.Areas.Admin.Controllers
     public class CategoriesController : Controller
     {
         private MyDBContext db = new MyDBContext();
-
-        // GET: Admin/Categories
+        //-------------------------------------------------------------------------------
+        // GET: Admin/Categories/Index
         public ActionResult Index()
         {
             return View(db.Categories.ToList());
         }
 
+        //-------------------------------------------------------------------------------
         // GET: Admin/Categories/Details/5
         public ActionResult Details(int? id)
         {
@@ -34,16 +35,13 @@ namespace _63CNTT4N1.Areas.Admin.Controllers
             }
             return View(categories);
         }
-
+        //-------------------------------------------------------------------------------
         // GET: Admin/Categories/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Admin/Categories/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Name,Slug,ParentId,Order,MetaDesc,MetaKey,CreateBy,CreateAt,UpdateBy,UpdateAt,Status")] Categories categories)
@@ -57,7 +55,7 @@ namespace _63CNTT4N1.Areas.Admin.Controllers
 
             return View(categories);
         }
-
+        //-------------------------------------------------------------------------------
         // GET: Admin/Categories/Edit/5
         public ActionResult Edit(int? id)
         {
@@ -73,9 +71,6 @@ namespace _63CNTT4N1.Areas.Admin.Controllers
             return View(categories);
         }
 
-        // POST: Admin/Categories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Name,Slug,ParentId,Order,MetaDesc,MetaKey,CreateBy,CreateAt,UpdateBy,UpdateAt,Status")] Categories categories)
@@ -89,6 +84,7 @@ namespace _63CNTT4N1.Areas.Admin.Controllers
             return View(categories);
         }
 
+        //-------------------------------------------------------------------------------
         // GET: Admin/Categories/Delete/5
         public ActionResult Delete(int? id)
         {
@@ -113,15 +109,6 @@ namespace _63CNTT4N1.Areas.Admin.Controllers
             db.Categories.Remove(categories);
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
