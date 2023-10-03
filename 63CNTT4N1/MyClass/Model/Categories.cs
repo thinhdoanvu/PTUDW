aebcf624e2eb0efc74f9ef5ec85d96a0e9383f35
thinhdoanvu <thinhdv@ntu.edu.vn>
2023-10-04 01:03:01 +0700

    Create_Categories

--- 63CNTT4N1/MyClass/Model/Categories.cs
+++ 63CNTT4N1/MyClass/Model/Categories.cs
@@ -15,33 +15,44 @@ namespace MyClass.Model
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Tên loại hàng")]
         public string Name { get; set; }
 
+        [Display(Name = "Tên rút gọn")]
         public string Slug { get; set; }
 
+        [Display(Name = "Cấp cha")]
         public int? ParentId { get; set; }
 
+        [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
 
         [Required]
+        [Display(Name = "Mô tả")]
         public string MetaDesc { get; set; }
 
         [Required]
+        [Display(Name = "Từ khóa")]
         public string MetaKey { get; set; }
 
         [Required]
+        [Display(Name = "Người tạo")]
         public int CreateBy { get; set; }
 
         [Required]
+        [Display(Name = "Ngày tạo")]
         public DateTime CreateAt { get; set; }
 
         [Required]
+        [Display(Name = "Người cập nhật")]
         public int UpdateBy { get; set; }
 
         [Required]
+        [Display(Name = "Ngày cập nhật")]
         public DateTime UpdateAt { get; set; }
 
         [Required]
+        [Display(Name = "Trạng thái")]
         public int Status { get; set; }
 
     }
